665d77e0e7103848d9981b396d62980762fec7ad
Fábio Lirio <oproffabio2022@gmail.com>
2026-02-03 15:00:38 -0300

    :rocket: deploy portfolio final version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-
-function updateProfileInfo(profileData) {
+function updateProfileInfo (profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
@@ -15,59 +14,70 @@ function updateProfileInfo(profileData) {
 
     const phone = document.getElementById('profile.phone')
     phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
+    phone.href = `Tel: ${profileData.phone}`
 
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
-    email.href = `mailto:${profileData.email}`
+    email.href = `mailto: ${profileData.email}`
+
 }
 
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
+
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="logo do ${skill.name}" title="${skill.name}"></li>`).join('')
+
 }
 
 function updateLanguages(profileData) {
     const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+
+    languages.innerHTML = profileData.languages.map(language => `<li> ${language} </li>`).join('')
 }
 
 function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
+
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return `
-            <li>
-                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-                <a href="${project.url}" target="_blank">${project.url}</a>
-            </li>
+        <li>
+        <h3 ${project.github ? 'class="github"' : ''} >${project.name}</h3>
+        <a href="${project.url}">${project.url}</a>
+        <p>${project.description}</p>
+        </li>
         `
-    }).join('')
+    }).join('') 
 }
 
-function updateProfessionalExperience(profileData) {
-    const professionalExperience = document.getElementById('profile.professionalExperience')
-    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+function updateExperience(profileData) {
+    const experiences = document.getElementById('profile.professionalExperience')
+
+    experiences.innerHTML = profileData.professionalExperience.map(experience => {
         return `
-            <li>
-                <h3 class="title">${experience.name}</h3>
-                <p class="period">${experience.period}</p>
-                <p>${experience.description}</p>
-            </li>
+        <li>
+            <h3>${experience.name}</h3>
+            <p ${experience.period ? 'class="period"' : ''}>${experience.period}</p>
+            <p>${experience.description}</p>
+        </li>
         `
     }).join('')
 }
 
-(async () => {
+(async function () {
     const profileData = await fetchProfileData()
+
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
     updatePortfolio(profileData)
-    updateProfessionalExperience(profileData)
+    updateExperience(profileData)
+
+
 })()
